51efad7d5b7532d7caf0d40fff551505bf64fef3
maimino <phuongmaivuu@gmail.com>
2024-02-22 16:30:11 +0700

    update file

---
+++ src/maimino/com/javabasic/toantu/ToanTuBit.java
@@ -0,0 +1,15 @@
+package maimino.com.javabasic.toantu;
+
+public class ToanTuBit {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 22;
+
+        //cach1
+        System.out.println((a>1) & (b>1));
+
+        //cach2
+        //boolean checkValue =  (a>1) & (b>1);
+        //System.out.println(checkValue);
+    }
+}
---
+++ src/maimino/com/javabasic/toantu/ToanTuSoHoc.java
@@ -0,0 +1,10 @@
+package maimino.com.javabasic.toantu;
+
+public class ToanTuSoHoc {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        System.out.println("a + b = " + (a+b));
+    }
+}
